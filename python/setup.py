2ab18db8db3a449bba4371332deeafd738d29298
David McNab <david@rebirthing.co.nz>
2003-12-12 04:21:31 +0000

    oops - forgot to add fuse/python/setup.py before - here it is!

---
+++ python/setup.py
@@ -0,0 +1,20 @@
+#@+leo-ver=4
+#@+node:@file setup.py
+"""
+distutils script for FUSE python module
+"""
+
+from distutils.core import setup, Extension
+
+setup(name="fuse",
+      version="0.1",
+      ext_modules=[Extension("_fusemodule", ["_fusemodule.c"],
+                             library_dirs=["../lib",],
+                             libraries=["fuse",],
+                             ),
+                   ],
+      py_modules=["fuse"],
+      )
+
+#@-node:@file setup.py
+#@-leo
