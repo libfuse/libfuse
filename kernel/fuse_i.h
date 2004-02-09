751a9a08c63fdd1efed88b186fe48514d30b2c92
Miklos Szeredi <miklos@szeredi.hu>
2004-02-09 09:54:09 +0000

    fix

--- kernel/fuse_i.h
+++ kernel/fuse_i.h
@@ -31,7 +31,8 @@
 #include <linux/list.h>
 #include <linux/spinlock.h>
 
-#ifndef KERNEL_2_6
+/* Some 2.4 kernels may have with the preemptible kernel patch */
+#if !defined(KERNEL_2_6) && !defined(CONFIG_PREEMPT)
 #define i_size_read(inode) ((inode)->i_size)
 #define i_size_write(inode, size) do { (inode)->i_size = size; } while(0)
 #endif
