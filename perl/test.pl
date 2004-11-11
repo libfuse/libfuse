30e8eb3e135291717eceaae69162fceb91d30aa1
Miklos Szeredi <miklos@szeredi.hu>
2004-11-11 15:20:05 +0000

    move perl python and lufis directories one level up

--- perl/test.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-BEGIN { $ENV{HARNESS_IGNORE_EXITCODE} = 1; }
-
-use Test::Harness qw(&runtests $verbose);
-$verbose=0;
-die "cannot find test directory!" unless -d "test";
-my (@files) = <test/*.t>;
-runtests("test/s/mount.t",sort(@files),"test/s/umount.t");
