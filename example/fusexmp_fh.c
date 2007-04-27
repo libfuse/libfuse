d2e5f49ba48c69e6d771a8f3d97e3706261347a2
Miklos Szeredi <miklos@szeredi.hu>
2007-04-27 19:24:35 +0000

    merge bugfix branch up to 2.6.4

--- example/fusexmp_fh.c
+++ example/fusexmp_fh.c
@@ -5,7 +5,7 @@
     This program can be distributed under the terms of the GNU GPL.
     See the file COPYING.
 
-    gcc -Wall `pkg-config fuse --cflags --libs` fusexmp_fh.c -o fusexmp_fh
+    gcc -Wall `pkg-config fuse --cflags --libs` -lulockmgr fusexmp_fh.c -o fusexmp_fh
 */
 
 #define FUSE_USE_VERSION 26
