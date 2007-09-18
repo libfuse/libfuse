8834fbf05b2f3a2fb46a3792c3912ece50ccfeab
Miklos Szeredi <miklos@szeredi.hu>
2007-09-18 11:34:14 +0000

    Add missing context initialization in fuse_fs_chmod()

--- lib/fuse.c
+++ lib/fuse.c
@@ -1425,6 +1425,7 @@ static void fuse_lib_getattr(fuse_req_t req, fuse_ino_t ino,
 
 int fuse_fs_chmod(struct fuse_fs *fs, const char *path, mode_t mode)
 {
+    fuse_get_context()->private_data = fs->user_data;
     if (fs->op.chmod)
         return fs->op.chmod(path, mode);
     else
