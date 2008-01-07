93bbd7fda1dd9ee975bce092f7edf10da479662a
Csaba Henk <csaba.henk@creo.hu>
2008-01-07 11:18:33 +0000

    make makeconf.sh work under FreeBSD

--- makeconf.sh
+++ makeconf.sh
@@ -3,7 +3,21 @@
 echo Running libtoolize...
 libtoolize --automake -c -f
 
-cp /usr/share/gettext/config.rpath .
+CONFIG_RPATH=/usr/share/gettext/config.rpath
+if ! [ -f $CONFIG_RPATH ]; then
+    CONFIG_RPATH=/usr/local/share/gettext/config.rpath
+fi
+if ! [ -f $CONFIG_RPATH ]; then
+    if  [ -f config.rpath ]; then
+        CONFIG_RPATH=
+    else
+        echo "config.rpath not found!" >&2
+        exit 1
+    fi
+fi
+if ! [ -z "$CONFIG_RPATH" ]; then
+    cp "$CONFIG_RPATH" .
+fi
 
 if test ! -z "`which autoreconf`"; then
     echo Running autoreconf...
