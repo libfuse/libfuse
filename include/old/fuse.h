d7d7ee9581a1a65527b9627e13bead27a429c777
Miklos Szeredi <mszeredi@suse.cz>
2012-07-19 19:03:53 +0200

    Remove include/old directory

--- include/old/fuse.h
+++
@@ -1,9 +0,0 @@
-/*
-   This header is for compatibility with older software using FUSE.
-
-   Please use 'pkg-config --cflags fuse' to set include path.  The
-   correct usage is still '#include <fuse.h>', not '#include
-   <fuse/fuse.h>'.
-*/
-
-#include "fuse/fuse.h"
