libfuse/libfuse

commit 4dc7e675bb17c7a126c78de75004877646d7605f

Don't unhash name in FORGET
This resulted in ENOENT being returned for unlinked but still open files if the
kernel sent a FORGET request for the parent directory.

Discovered with fs_racer in LTP.

lib/fuse.c
@@ -775,7 +775,7 @@ static void delete_node(struct fuse *f, struct node *node)
 			(unsigned long long) node->nodeid);
 
 	assert(node->treelock == 0);
-	assert(!node->name);
+	unhash_name(f, node);
 	if (lru_enabled(f))
 		remove_node_lru(node);
 	unhash_id(f, node);
@@ -1258,7 +1258,6 @@ static void forget_node(struct fuse *f, fuse_ino_t nodeid, uint64_t nlookup)
 	assert(node->nlookup >= nlookup);
 	node->nlookup -= nlookup;
 	if (!node->nlookup) {
-		unhash_name(f, node);
 		unref_node(f, node);
 	} else if (lru_enabled(f) && node->nlookup == 1) {
 		set_forget_time(f, node);

commit 1061a0a2d90148bd2e7f32e1e694399db2dbe087

Fix "fuse internal error: node NNN not found"
Commit 4dc7e675bb (Don't unhash name in FORGET) broke the forget logic in a
subtle way, resulting in "fuse internal error: node NNN not found" and causing
the filesystem daemon to abort.

Fix by incrementing the node refcount if nlookup goes from zero to one.

Reported by Kyle Lippincott

lib/fuse.c
@@ -816,6 +816,13 @@ static struct node *lookup_node(struct fuse *f, fuse_ino_t parent,
 	return NULL;
 }
 
+static void inc_nlookup(struct node *node)
+{
+	if (!node->nlookup)
+		node->refctr++;
+	node->nlookup++;
+}
+
 static struct node *find_node(struct fuse *f, fuse_ino_t parent,
 			      const char *name)
 {
@@ -831,15 +838,16 @@ static struct node *find_node(struct fuse *f, fuse_ino_t parent,
 		if (node == NULL)
 			goto out_err;
 
-		if (f->conf.remember)
-			node->nlookup = 1;
-		node->refctr = 1;
+		node->refctr = 0;
 		node->nodeid = next_id(f);
 		node->generation = f->generation;
 		node->open_count = 0;
 		node->is_hidden = 0;
 		node->treelock = 0;
 		node->ticket = 0;
+		if (f->conf.remember)
+			inc_nlookup(node);
+
 		if (hash_name(f, node, parent, name) == -1) {
 			free_node(f, node);
 			node = NULL;
@@ -853,7 +861,7 @@ static struct node *find_node(struct fuse *f, fuse_ino_t parent,
 	} else if (lru_enabled(f) && node->nlookup == 1) {
 		remove_node_lru(node);
 	}
-	node->nlookup ++;
+	inc_nlookup(node);
 out_err:
 	pthread_mutex_unlock(&f->lock);
 	return node;