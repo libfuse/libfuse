57d7f5da5506216427886df6ca0e9f2b8b1a25a2
Miklos Szeredi <mszeredi@suse.cz>
2014-03-18 12:01:45 +0100

    compile fix for fuse_lo-plus

--- example/fuse_lo-plus.c
+++ example/fuse_lo-plus.c
@@ -29,6 +29,48 @@
 #include <errno.h>
 #include <err.h>
 
+/* Compat stuff.  Doesn't make it work, just makes it compile. */
+#ifndef HAVE_FSTATAT
+#warning fstatat(2) needed by this program
+int fstatat(int dirfd, const char *pathname, struct stat *buf, int flags)
+{
+	errno = ENOSYS;
+	return -1;
+}
+#endif
+#ifndef HAVE_OPENAT
+#warning openat(2) needed by this program
+int openat(int dirfd, const char *pathname, int flags, ...)
+{
+	errno = ENOSYS;
+	return -1;
+}
+#endif
+#ifndef HAVE_READLINKAT
+#warning readlinkat(2) needed by this program
+ssize_t readlinkat(int dirfd, const char *pathname, char *buf, size_t bufsiz)
+{
+	errno = ENOSYS;
+	return -1;
+}
+#endif
+#ifndef AT_EMPTY_PATH
+#warning AT_EMPTY_PATH needed by this program
+#define AT_EMPTY_PATH 0
+#endif
+#ifndef AT_SYMLINK_NOFOLLOW
+#warning AT_SYMLINK_NOFOLLOW needed by this program
+#define AT_SYMLINK_NOFOLLOW 0
+#endif
+#ifndef O_PATH
+#warning O_PATH needed by this program
+#define O_PATH 0
+#endif
+#ifndef O_NOFOLLOW
+#warning O_NOFOLLOW needed by this program
+#define O_NOFOLLOW 0
+#endif
+
 struct lo_inode {
 	struct lo_inode *next;
 	struct lo_inode *prev;
