b8a82603bfea20b8edc17531ed2209a753ae8417
Nikolaus Rath <Nikolaus@rath.org>
2016-01-14 10:14:35 -0800

    Update makeconf.sh
    Describe why manual copying of config.rpath is necessary, and fail
    with a more helpful message if it can't be found.
    
    Remove code for systems without autoreconf - it's apparently not used
    by anyone since it has been broken for quite some time (there is no
    `kernel` directory anymore).

--- makeconf.sh
+++ makeconf.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
-echo Running libtoolize...
-libtoolize --automake -c -f
+echo "Running libtoolize..."
+libtoolize -c
 
+# We use iconv directly rather than via gettext, so
+# we need to manually copy config.rpath.
 CONFIG_RPATH=/usr/share/gettext/config.rpath
 if ! [ -f $CONFIG_RPATH ]; then
     CONFIG_RPATH=/usr/local/share/gettext/config.rpath
@@ -11,7 +13,7 @@ if ! [ -f $CONFIG_RPATH ]; then
     if  [ -f config.rpath ]; then
         CONFIG_RPATH=
     else
-        echo "config.rpath not found!" >&2
+        echo "config.rpath not found! - is gettext installed?" >&2
         exit 1
     fi
 fi
@@ -19,27 +21,6 @@ if ! [ -z "$CONFIG_RPATH" ]; then
     cp "$CONFIG_RPATH" .
 fi
 
-if test ! -z "`which autoreconf`"; then
-    echo Running autoreconf...
-    autoreconf -i -f
-else
-    echo Running aclocal...
-    aclocal
-    echo Running autoheader...
-    autoheader
-    echo Running autoconf...
-    autoconf
-    echo Running automake...
-    automake -a -c
-    (
-	echo Entering directory: kernel
-	cd kernel
-	echo Running autoheader...
-	autoheader
-	echo Running autoconf...
-	autoconf
-    )
-fi
+echo "Running autoreconf..."
+autoreconf -i
 
-rm -f config.cache config.status
-echo "To compile run './configure', and then 'make'."
