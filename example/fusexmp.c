06520349dcb419822898dd693a8757dc8c8a184a
Seth Forshee <seth.forshee@canonical.com>
2016-08-23 10:38:38 -0500

    fusexmp: Add ACL support
    Add support for ACLs in fusexmp using the posix ACl xattr layout
    but different xattr names.
    
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- example/fusexmp.c
+++ example/fusexmp.c
@@ -33,6 +33,7 @@
 #endif
 
 #include <fuse.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -41,9 +42,19 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <libgen.h>
 #ifdef HAVE_SETXATTR
 #include <sys/xattr.h>
 #endif
+
+struct xmp_data {
+	int acls;
+};
+
+static int xmp_setacl(const char *path, enum posix_acl_type type,
+		      struct fuse_acl *acl);
+static int xmp_getacl(const char *path, enum posix_acl_type type,
+		      struct fuse_acl **acl);
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
@@ -108,9 +119,47 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	return 0;
 }
 
+static int xmp_acl_mknod(const char *path, mode_t *pmode, bool is_dir,
+			 struct fuse_acl **default_acl,
+			 struct fuse_acl **access_acl)
+{
+	struct fuse_acl *p = NULL, *d = NULL, *a = NULL;
+	char *tmp, *parent_path;
+	int res;
+
+	tmp = strdup(path);
+	if (!tmp)
+		return -errno;
+	parent_path = dirname(tmp);
+	res = xmp_getacl(parent_path, POSIX_ACL_TYPE_DEFAULT, &p);
+	free(tmp);
+	if (res == -ENODATA)
+		goto out;
+	if (res)
+		return res;
+	res = fuse_acl_mknod(p, pmode, is_dir, &d, &a);
+	free(p);
+	if (res)
+		return res;
+
+out:
+	*access_acl = a;
+	*default_acl = d;
+	return 0;
+}
+
 static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
 {
+	struct xmp_data *xmp = fuse_get_context()->private_data;
 	int res;
+	struct fuse_acl *default_acl = NULL, *access_acl = NULL;
+
+	if (xmp->acls) {
+		res = xmp_acl_mknod(path, &mode, false, &default_acl,
+				    &access_acl);
+		if (res)
+			return res;
+	}
 
 	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
 	   is more portable */
@@ -122,21 +171,62 @@ static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
 		res = mkfifo(path, mode);
 	else
 		res = mknod(path, mode, rdev);
-	if (res == -1)
-		return -errno;
+	if (res == -1) {
+		res = -errno;
+		goto out;
+	}
 
-	return 0;
+	if (default_acl) {
+		res = xmp_setacl(path, POSIX_ACL_TYPE_DEFAULT, default_acl);
+		if (res)
+			goto out;
+	}
+	if (access_acl) {
+		res = xmp_setacl(path, POSIX_ACL_TYPE_ACCESS, access_acl);
+		if (res)
+			goto out;
+	}
+
+out:
+	free(default_acl);
+	free(access_acl);
+	return res;
 }
 
 static int xmp_mkdir(const char *path, mode_t mode)
 {
+	struct xmp_data *xmp = fuse_get_context()->private_data;
+	struct fuse_acl *default_acl = NULL, *access_acl = NULL;
 	int res;
 
-	res = mkdir(path, mode);
-	if (res == -1)
-		return -errno;
+	if (xmp->acls) {
+		res = xmp_acl_mknod(path, &mode, true, &default_acl,
+				    &access_acl);
+		if (res)
+			return res;
+	}
 
-	return 0;
+	res = mkdir(path, mode);
+	if (res == -1) {
+		res = -errno;
+		goto out;
+	}
+
+	if (default_acl) {
+		res = xmp_setacl(path, POSIX_ACL_TYPE_DEFAULT, default_acl);
+		if (res)
+			goto out;
+	}
+	if (access_acl) {
+		res = xmp_setacl(path, POSIX_ACL_TYPE_ACCESS, access_acl);
+		if (res)
+			goto out;
+	}
+
+out:
+	free(default_acl);
+	free(access_acl);
+	return res;
 }
 
 static int xmp_unlink(const char *path)
@@ -199,7 +289,23 @@ static int xmp_link(const char *from, const char *to)
 
 static int xmp_chmod(const char *path, mode_t mode)
 {
+	struct xmp_data *xmp = fuse_get_context()->private_data;
 	int res;
+
+	if (xmp->acls) {
+		struct fuse_acl *acl;
+		res = xmp_getacl(path, POSIX_ACL_TYPE_ACCESS, &acl);
+		if (!res) {
+			res = fuse_acl_chmod(acl, mode);
+			if (!res)
+				res = xmp_setacl(path, POSIX_ACL_TYPE_ACCESS, acl);
+			free(acl);
+			if (res)
+				return res;
+		} else if (res != -ENODATA) {
+			return res;
+		}
+	}
 
 	res = chmod(path, mode);
 	if (res == -1)
@@ -385,9 +491,122 @@ static int xmp_removexattr(const char *path, const char *name)
 		return -errno;
 	return 0;
 }
+
+/*
+ * Data of ACL xattrs is the same as standard posix ACLs, but use
+ * different xattr names so that the underlying filesystem doesn't
+ * treat them as ACLs.
+ */
+#define XMP_ACCESS_ACL_XATTR	"user.fusexmp_acl_access"
+#define XMP_DEFAULT_ACL_XATTR	"user.fusexmp_acl_default"
+
+static int xmp_setacl(const char *path, enum posix_acl_type type,
+		      struct fuse_acl *acl)
+{
+	struct xmp_data *xmp = fuse_get_context()->private_data;
+	const char *name;
+	struct posix_acl_xattr *xattr;
+	ssize_t size;
+	uint32_t mode;
+	int ret = 0;
+
+	if (!xmp->acls)
+		return -ENOSYS;
+
+	switch (type) {
+	case POSIX_ACL_TYPE_ACCESS:
+		name = XMP_ACCESS_ACL_XATTR;
+		if (acl) {
+			ret = fuse_acl_equiv_mode(acl, &mode);
+			if (ret < 0)
+				return ret;
+		}
+		break;
+	case POSIX_ACL_TYPE_DEFAULT:
+		name = XMP_DEFAULT_ACL_XATTR;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	if (!acl)
+		return xmp_removexattr(path, name);
+
+	size = fuse_acl_to_xattr(acl, &xattr);
+	if (size < 0)
+		return (int)size;
+	ret = xmp_setxattr(path, name, (char *)xattr, size, 0);
+	if (!ret && type == POSIX_ACL_TYPE_ACCESS) {
+		/* Don't use xmp_chmod() as it will try to update the ACL */
+		if (chmod(path, mode) == -1)
+			ret = -errno;
+	}
+	free(xattr);
+	return ret;
+}
+
+static int xmp_getacl(const char *path, enum posix_acl_type type,
+		      struct fuse_acl **acl)
+{
+	struct xmp_data *xmp = fuse_get_context()->private_data;
+	const char *name;
+	ssize_t size;
+	struct posix_acl_xattr *xattr;
+	int ret;
+
+	if (!xmp->acls)
+		return -ENODATA;
+
+	switch (type) {
+	case POSIX_ACL_TYPE_ACCESS:
+		name = XMP_ACCESS_ACL_XATTR;
+		break;
+	case POSIX_ACL_TYPE_DEFAULT:
+		name = XMP_DEFAULT_ACL_XATTR;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	size = lgetxattr(path, name, NULL, 0);
+	if (size == -1)
+		return -errno;
+	xattr = malloc(size);
+	if (!xattr)
+		return -ENOMEM;
+
+	size = lgetxattr(path, name, xattr, size);
+	if (size == -1)
+		ret = -errno;
+	else
+		ret = fuse_acl_from_xattr(xattr, size, acl);
+	free(xattr);
+	return ret;
+}
+#else
+static int xmp_setacl(const char *path, enum posix_acl_type type,
+		      struct fuse_acl *acl)
+{
+	return -ENOSYS;
+}
+
+
+static int xmp_getacl(const char *path, enum posix_acl_type type,
+		      struct fuse_acl **acl)
+{
+	return -ENODATA;
+}
 #endif /* HAVE_SETXATTR */
 
+static void *xmp_init(struct fuse_conn_info *conn)
+{
+	struct xmp_data *xmp = fuse_get_context()->private_data;
+	xmp->acls = (conn->want & FUSE_CAP_POSIX_ACL) != 0;
+	return xmp;
+}
+
 static struct fuse_operations xmp_oper = {
+	.init		= xmp_init,
 	.getattr	= xmp_getattr,
 	.access		= xmp_access,
 	.readlink	= xmp_readlink,
@@ -419,11 +638,14 @@ static struct fuse_operations xmp_oper = {
 	.getxattr	= xmp_getxattr,
 	.listxattr	= xmp_listxattr,
 	.removexattr	= xmp_removexattr,
+	.setacl		= xmp_setacl,
+	.getacl		= xmp_getacl,
 #endif
 };
 
 int main(int argc, char *argv[])
 {
+	struct xmp_data xmp = {0,};
 	umask(0);
-	return fuse_main(argc, argv, &xmp_oper, NULL);
+	return fuse_main(argc, argv, &xmp_oper, &xmp);
 }
