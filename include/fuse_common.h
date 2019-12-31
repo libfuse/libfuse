2cfce3990904528788db674186986cc069e03ed2
Nikolaus Rath <Nikolaus@rath.org>
2019-12-31 09:59:28 +0000

    Bump FUSE_MINOR_VERSION
    This was forgotten over several releases.

--- include/fuse_common.h
+++ include/fuse_common.h
@@ -23,7 +23,7 @@
 #define FUSE_MAJOR_VERSION 3
 
 /** Minor version of FUSE library interface */
-#define FUSE_MINOR_VERSION 2
+#define FUSE_MINOR_VERSION 9
 
 #define FUSE_MAKE_VERSION(maj, min)  ((maj) * 10 + (min))
 #define FUSE_VERSION FUSE_MAKE_VERSION(FUSE_MAJOR_VERSION, FUSE_MINOR_VERSION)
