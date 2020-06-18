f970697724e76a769e037434fbf0e834caa984e0
Albert Chen <hselin@kalista.io>
2020-06-17 17:09:28 -0700

    Fixed minor print alignment issue in iconv_help(), replacing tab with space

--- lib/modules/iconv.c
+++ lib/modules/iconv.c
@@ -644,7 +644,7 @@ static void iconv_help(void)
 	free(old);
 	printf(
 "    -o from_code=CHARSET   original encoding of file names (default: UTF-8)\n"
-"    -o to_code=CHARSET	    new encoding of the file names (default: %s)\n",
+"    -o to_code=CHARSET     new encoding of the file names (default: %s)\n",
 		charmap);
 	free(charmap);
 }
