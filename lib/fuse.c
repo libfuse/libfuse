5d38afc8a5b4a2a6e27aad7a1840046e99cd826d
Andrew Gaul <gaul@google.com>
2020-12-14 19:16:05 +0900

    Correct errno comparison (#571)

--- lib/fuse.c
+++ lib/fuse.c
@@ -4350,7 +4350,7 @@ static int fuse_session_loop_remember(struct fuse *f)
 
 		res = poll(&fds, 1, timeout * 1000);
 		if (res == -1) {
-			if (errno == -EINTR)
+			if (errno == EINTR)
 				continue;
 			else
 				break;
