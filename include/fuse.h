dad15aee26835240d72188179ba177a0bbafe659
Amir Goldstein <amir73il@gmail.com>
2021-12-05 18:29:05 +0200

    Add no_rofd_flush mount option
    To disable flush for read-only fd.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- include/fuse.h
+++ include/fuse.h
@@ -248,6 +248,14 @@ struct fuse_config {
 	int auto_cache;
 
 	/**
+	 * By default, fuse waits for all pending writes to complete
+	 * and calls the FLUSH operation on close(2) of every fuse fd.
+	 * With this option, wait and FLUSH are not done for read-only
+	 * fuse fd, similar to the behavior of NFS/SMB clients.
+	 */
+	int no_rofd_flush;
+
+	/**
 	 * The timeout in seconds for which file attributes are cached
 	 * for the purpose of checking if auto_cache should flush the
 	 * file data on open.
