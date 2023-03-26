b509964f7cc7260c16abb41bf4ccabdce08c6bce
Nikolaus Rath <Nikolaus@rath.org>
2023-03-26 11:49:40 +0100

    Update script to drop references to Travis CI.

--- make_release_tarball.sh
+++ make_release_tarball.sh
@@ -21,9 +21,9 @@ mkdir "${TAG}"
 
 git archive --format=tar "${TAG}" | tar -x "--directory=${TAG}"
 find "${TAG}" -name .gitignore -delete
-rm "${TAG}/make_release_tarball.sh" \
-   "${TAG}/.travis.yml" \
-   "${TAG}/.cirrus.yml"
+rm -r "${TAG}/make_release_tarball.sh" \
+      "${TAG}/.github" \
+      "${TAG}/.cirrus.yml"
 cp -a doc/html "${TAG}/doc/"
 tar -cJf "${TAG}.tar.xz" "${TAG}/"
 gpg --armor --detach-sign "${TAG}.tar.xz"
