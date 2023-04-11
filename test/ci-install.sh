dd2c951607bdbdb21270affeca631738c7035d1c
Matthias Goergens <matthias.goergens@gmail.com>
2023-04-11 19:08:40 +0800

    Migrate away from deprecated distutils
    Before:
    
    ```
    =============================== warnings summary ===============================
    test/test_ctests.py:12
      /tmp/libfuse-build-mB50ZC/build-gcc-9/test/test_ctests.py:12: DeprecationWarning: The distutils package is deprecated and slated for removal in Python 3.12. Use setuptools or check PEP 632 for potential alternatives
        from distutils.version import LooseVersion
    
    -- Docs: https://docs.pytest.org/en/stable/how-to/capture-warnings.html
    ```

--- test/ci-install.sh
+++ test/ci-install.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install pytest meson==1.0.1
+sudo python3 -m pip install pytest meson==1.0.1 looseversion
 valgrind --version
 ninja --version
 meson --version
