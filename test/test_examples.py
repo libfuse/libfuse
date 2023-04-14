a39f4370fca1096d6fa6e90267c9243a832d9d8c
Peri <peri@srdi.org>
2023-04-14 15:44:35 +0000

    Added #746 test in test_examples.py.

--- test/test_examples.py
+++ test/test_examples.py
@@ -442,6 +442,97 @@ def test_cuse(output_checker):
     finally:
         mount_process.terminate()
 
+def test_746(tmpdir, output_checker):
+    # ###
+    # ### test case for Issue #746
+    # ###
+    #
+    # If RELEASE and UNLINK opcodes are sent back to back, and fuse_fs_release()
+    # and fuse_fs_rename() are slow to execute, UNLINK will run while RELEASE is
+    # still executing, UNLINK will try to rename the file, and while the rename
+    # is happening the RELEASE will finish executing. So at the end, RELEASE will
+    # not detect in time that UNLINK has happened, and UNLINK will not detect in
+    # time that RELEASE has happened.
+    #
+    # NOTE: This is triggered only when nullpath_ok is set.
+    #
+    # If it is not SET then get_path_nullok() called by fuse_lib_release() will
+    # call get_path_common() and lock the path, and then the fuse_lib_unlink()
+    # will wait for the path to be unlocked before executing and thus synchronise
+    # with fuse_lib_release().
+    #
+    # If it is SET then get_path_nullok() will just set the path to null and
+    # return without locking anything and thus allowing fuse_lib_unlink() to
+    # eventually execute unimpeded while fuse_lib_release() is still running.
+
+    ## Normal run:
+    # python3 test746.py
+    #
+    ## Disable release() & rename() runtime delay injection:
+    # TEST746_DELAY_DISABLE=1 python3 test746.py
+
+    # set the path for the library to preload
+    preloadLib = base_cmdline + \
+        [ pjoin(basename, "test", "test746.so") ]
+    preloadLib = preloadLib[0]
+    print("preloadLib: ", preloadLib)
+
+    # create the FUSE mountpoint
+    fuseMNT = str(tmpdir)
+    print("fuseMNT: ", fuseMNT)
+    os.makedirs(fuseMNT, exist_ok=True)
+
+    # set the FUSE binary path
+    fuseCMD = base_cmdline + \
+        [ pjoin(basename, 'example', 'passthrough_fh'),
+        "-f", fuseMNT]
+    print("fuseCMD: ", str(fuseCMD))
+
+    # start FUSE with out preload library
+    os.environ["LD_PRELOAD"] = preloadLib
+    fuseProcess = subprocess.Popen(fuseCMD,
+                                   stdout=output_checker.fd,
+                                   stderr=output_checker.fd)
+    os.environ["LD_PRELOAD"] = ""
+
+    ret = -1
+    try:
+        wait_for_mount(fuseProcess, fuseMNT)
+
+        # use TemporaryDirectory so that it gets cleaned up at the end automatically
+        tempDir = tempfile.TemporaryDirectory(dir=fuseMNT + "/tmp/")
+        tempDirPath = tempDir.name
+        #print("tempDir: " + tempDirPath);
+
+        tempFile, tempFilePath = tempfile.mkstemp(dir=tempDir.name)
+        #print("tempFile: " + tempFilePath);
+
+        # test for the race condition
+        os.close(tempFile)
+        os.unlink(tempFilePath)
+
+        # check if any leftover files are still there
+        tempDirFiles = os.listdir(tempDirPath)
+        #print(tempDirFiles)
+
+        if len(tempDirFiles) == 0:
+            print(" *** TEST746 PASS ***")
+            ret = 0
+        else:
+            print(" *** TEST746 FAIL ***")
+            ret = 1
+
+    except:
+        cleanup(fuseProcess, fuseMNT)
+
+    else:
+        umount(fuseProcess, fuseMNT)
+
+    os.removedirs(fuseMNT);
+
+    assert ret == 0
+
+
 @contextmanager
 def os_open(name, flags):
     fd = os.open(name, flags)
