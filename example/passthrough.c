5ebb4bae2b87d5f66e688dc3597b22e43cff9b27
Bernd Schubert <bschubert@ddn.com>
2023-10-10 13:18:36 +0200

    Enabled parallel direct IO writes for passthrough examples
    All these passthrough examples don't need writes to be serialized.
    
    Actually, most file systems probably handle non serialized parallel
    direct writes - the FOPEN_PARALLEL_DIRECT_WRITES flag is just
    to avoid a regression for those file system that rely on serialized
    DIO writes in fuse kernel. Passthrough file system forward the IO
    to another file system, which actually handles that internally -
    serialized in fuser kernel is not needed.

--- example/passthrough.c
+++ example/passthrough.c
@@ -285,6 +285,7 @@ static int xmp_create(const char *path, mode_t mode,
 		return -errno;
 
 	fi->fh = res;
+	fi->parallel_direct_writes = 1;
 	return 0;
 }
 
@@ -297,6 +298,7 @@ static int xmp_open(const char *path, struct fuse_file_info *fi)
 		return -errno;
 
 	fi->fh = res;
+	fi->parallel_direct_writes = 1;
 	return 0;
 }
 
