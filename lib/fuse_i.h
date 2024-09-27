296916ee18e6dd5a2bb76dc9ffc144074f6250b5
Joanne Koong <joannelkoong@gmail.com>
2024-09-26 17:33:39 -0700

    Change FUSE_MAX_MAX_PAGES to FUSE_DEFAULT_MAX_PAGES_LIMIT
    A recent upstream patch [1] changed FUSE_MAX_MAX_PAGES to
    FUSE_DEFAULT_MAX_PAGES_LIMIT.
    
    Update libfuse to use FUSE_DEFAULT_MAX_PAGES_LIMIT as well
    instead of FUSE_MAX_MAX_PAGES.
    
    [1] https://lore.kernel.org/linux-fsdevel/20240923171311.1561917-1-joannelkoong@gmail.com/T/#t

--- lib/fuse_i.h
+++ lib/fuse_i.h
@@ -196,7 +196,13 @@ int fuse_session_loop_mt_312(struct fuse_session *se, struct fuse_loop_config *c
 int fuse_loop_cfg_verify(struct fuse_loop_config *config);
 
 
-#define FUSE_MAX_MAX_PAGES 256
+/*
+ * This can be changed dynamically on recent kernels through the
+ * /proc/sys/fs/fuse/max_pages_limit interface.
+ *
+ * Older kernels will always use the default value.
+ */
+#define FUSE_DEFAULT_MAX_PAGES_LIMIT 256
 #define FUSE_DEFAULT_MAX_PAGES_PER_REQ 32
 
 /* room needed in buffer to accommodate header */
