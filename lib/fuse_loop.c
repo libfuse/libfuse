c9b9ca09e0e5e4fc3c648eda795f45122a1c323b
Bernd Schubert <bschubert@ddn.com>
2024-12-17 20:21:09 +0100

    Rename _int to _internal
    _int can be confused with 'integer'

--- lib/fuse_loop.c
+++ lib/fuse_loop.c
@@ -24,7 +24,7 @@ int fuse_session_loop(struct fuse_session *se)
 	};
 
 	while (!fuse_session_exited(se)) {
-		res = fuse_session_receive_buf_int(se, &fbuf, NULL);
+		res = fuse_session_receive_buf_internal(se, &fbuf, NULL);
 
 		if (res == -EINTR)
 			continue;
