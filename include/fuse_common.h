2eb7767e2c2a4e6f9f21f7b10f79d4860ebe2831
Bernd Schubert <bernd@bsbernd.com>
2024-12-28 13:19:41 +0100

    Use (u)int32_t for struct fuse_file_info
    Enforce 32 bit integers for #struct fuse_file_info'.
    
    Signed-off-by: Bernd Schubert <bernd@bsbernd.com>

--- include/fuse_common.h
+++ include/fuse_common.h
@@ -48,7 +48,7 @@ extern "C" {
  */
 struct fuse_file_info {
 	/** Open flags.	 Available in open(), release() and create() */
-	int flags;
+	int32_t flags;
 
 	/** In case of a write operation indicates if this was caused
 	    by a delayed write from the page cache. If so, then the
@@ -56,44 +56,44 @@ struct fuse_file_info {
 	    the *fh* value may not match the *fh* value that would
 	    have been sent with the corresponding individual write
 	    requests if write caching had been disabled. */
-	unsigned int writepage : 1;
+	uint32_t writepage : 1;
 
 	/** Can be filled in by open/create, to use direct I/O on this file. */
-	unsigned int direct_io : 1;
+	uint32_t direct_io : 1;
 
 	/** Can be filled in by open and opendir. It signals the kernel that any
 	    currently cached data (ie., data that the filesystem provided the
 	    last time the file/directory was open) need not be invalidated when
 	    the file/directory is closed. */
-	unsigned int keep_cache : 1;
+	uint32_t keep_cache : 1;
 
 	/** Indicates a flush operation.  Set in flush operation, also
 	    maybe set in highlevel lock operation and lowlevel release
 	    operation. */
-	unsigned int flush : 1;
+	uint32_t flush : 1;
 
 	/** Can be filled in by open, to indicate that the file is not
 	    seekable. */
-	unsigned int nonseekable : 1;
+	uint32_t nonseekable : 1;
 
 	/* Indicates that flock locks for this file should be
 	   released.  If set, lock_owner shall contain a valid value.
 	   May only be set in ->release(). */
-	unsigned int flock_release : 1;
+	uint32_t flock_release : 1;
 
 	/** Can be filled in by opendir. It signals the kernel to
 	    enable caching of entries returned by readdir().  Has no
 	    effect when set in other contexts (in particular it does
 	    nothing when set by open()). */
-	unsigned int cache_readdir : 1;
+	uint32_t cache_readdir : 1;
 
 	/** Can be filled in by open, to indicate that flush is not needed
 	    on close. */
-	unsigned int noflush : 1;
+	uint32_t noflush : 1;
 
 	/** Can be filled by open/create, to allow parallel direct writes on this
 	    file */
-	unsigned int parallel_direct_writes : 1;
+	uint32_t parallel_direct_writes : 1;
 
 	/** Padding.  Reserved for future use*/
 	unsigned int padding : 23;
