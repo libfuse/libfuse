387a2da78f8f8eba22b43a0f4fed23cf7fb1ecc2
Bernd Schubert <bschubert@ddn.com>
2025-03-26 01:07:05 +0100

    Add fuse_session_process_uring_cqe
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>

--- lib/fuse_uring_i.h
+++ lib/fuse_uring_i.h
@@ -19,4 +19,9 @@ struct fuse_in_header;
 int fuse_uring_start(struct fuse_session *se);
 int fuse_uring_stop(struct fuse_session *se);
 
+void fuse_session_process_uring_cqe(struct fuse_session *se,
+				    struct fuse_req *req,
+				    struct fuse_in_header *in, void *in_header,
+				    void *in_payload, size_t payload_len);
+
 #endif // FUSE_URING_I_H_
