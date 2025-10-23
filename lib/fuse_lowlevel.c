b9c8a1c580a27a2e38055c9d4e431baede767317
Joanne Koong <joannelkoong@gmail.com>
2025-10-23 09:55:28 -0700

    init: fix io-uring mount hang
    If libfuse cannot set up the server to use io-uring, then it should not
    send FUSE_OVER_IO_URING in the init reply.
    
    outarg.flags needs to be set after FUSE_OVER_IO_URING is unset in the
    error case where io-uring setup fails, else the mount will hang.
    
    Signed-off-by: Joanne Koong <joannelkoong@gmail.com>

--- lib/fuse_lowlevel.c
+++ lib/fuse_lowlevel.c
@@ -2775,13 +2775,6 @@ _do_init(fuse_req_t req, const fuse_ino_t nodeid, const void *op_in,
 		outarg.align_page_order = se->conn.align_page_order;
 	}
 
-	if (inargflags & FUSE_INIT_EXT) {
-		outargflags |= FUSE_INIT_EXT;
-		outarg.flags2 = outargflags >> 32;
-	}
-
-	outarg.flags = outargflags;
-
 	if (inargflags & FUSE_URING_REDUCED_Q) {
 		outargflags |= FUSE_URING_REDUCED_Q;
 	}
@@ -2830,6 +2823,12 @@ _do_init(fuse_req_t req, const fuse_ino_t nodeid, const void *op_in,
 		outargsize = FUSE_COMPAT_22_INIT_OUT_SIZE;
 
 
+
+	if (inargflags & FUSE_INIT_EXT) {
+		outargflags |= FUSE_INIT_EXT;
+		outarg.flags2 = outargflags >> 32;
+	}
+	outarg.flags = outargflags;
 
 	send_reply_ok(req, &outarg, outargsize);
 
