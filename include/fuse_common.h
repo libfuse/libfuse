1f4c0608e7c6202f88db791c19cc4bfe60d40edd
Renzheng Wang <renzheng.wang@ucloud.cn>
2026-03-15 19:55:32 +0800

    add FUSE_CAP_ALLOW_IDMAP (#1446)
    * fuse: add FUSE_CAP_ALLOW_IDMAP support
    
    Add FUSE_CAP_ALLOW_IDMAP capability flag to indicate that creation of
    idmapped mounts is allowed. This maps to the kernel's FUSE_ALLOW_IDMAP
    flag.
    
    - Define FUSE_CAP_ALLOW_IDMAP in fuse_common.h using (1ULL << 32) to
      ensure correct behavior on both 32-bit and 64-bit platforms.
    - Set the capability in capable_ext when kernel reports FUSE_ALLOW_IDMAP.
    - Send FUSE_ALLOW_IDMAP to kernel when userspace sets it in want_ext.
    - Add the new FUSE_CAP_ALLOW_IDMAP capability to the printcap example
    so it is printed when supported by the kernel.
    
    Signed-off-by: Renzheng Wang <wangrzneu@gmail.com>

--- include/fuse_common.h
+++ include/fuse_common.h
@@ -513,6 +513,11 @@ struct fuse_loop_config_v1 {
 #define FUSE_CAP_OVER_IO_URING (1UL << 31)
 
 /**
+ * Indicates that creation of idmapped mounts is allowed
+ */
+#define FUSE_CAP_ALLOW_IDMAP (1ULL << 32)
+
+/**
  * Ioctl flags
  *
  * FUSE_IOCTL_COMPAT: 32bit compat ioctl on 64bit machine
