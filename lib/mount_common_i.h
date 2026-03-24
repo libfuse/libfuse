267476a4d260b2c9d57b0256181a996e7a73698c
Bernd Schubert <bschubert@ddn.com>
2026-03-24 10:53:22 -0700

    Refactor mount code / move common functions to mount_util.c
    Also create the new "mount_i.h", which is independent of the
    the rest of libfuse.
    
    This is preparation for the new mount API, which goes into a new file.
    
    This is to allow fusermount to use the code from mount_fsmount.c.
    I.e. avoid code dup and add just re-use the new linux api mount
    functions from that file for fusermount.
    
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    [djwong: extract only the parts we need for mount services]
    Reviewed-by: "Darrick J. Wong" <djwong@kernel.org>
    Signed-off-by: "Darrick J. Wong" <djwong@kernel.org>

---
+++ lib/mount_common_i.h
@@ -0,0 +1,18 @@
+/*
+ *  FUSE: Filesystem in Userspace
+ *  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+ *                2026 Bernd Schubert <bernd@bsbernd.com>
+ *
+ *  This program can be distributed under the terms of the GNU LGPLv2.
+ *  See the file LGPL2.txt
+ */
+
+#ifndef FUSE_MOUNT_COMMON_I_H_
+#define FUSE_MOUNT_COMMON_I_H_
+
+struct mount_opts;
+
+char *fuse_mnt_build_source(const struct mount_opts *mo);
+char *fuse_mnt_build_type(const struct mount_opts *mo);
+
+#endif /* FUSE_MOUNT_COMMON_I_H_ */
