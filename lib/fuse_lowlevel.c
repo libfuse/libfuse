6fb033078b7c95e47f4a6e38f68ec659d19deb55
Abhinav Agarwal <abhinavagarwal1996@gmail.com>
2026-04-15 10:15:27 -0700

    fuse_lowlevel: Add O_CLOEXEC to pipe-max-size procfs open
    grow_pipe_to_max() opens /proc/sys/fs/pipe-max-size without O_CLOEXEC.
    In a multi-threaded FUSE daemon, a concurrent fork+exec (e.g., to
    invoke fusermount) between the open() and close() calls would leak this
    fd into the child process.
    
    Add the same O_CLOEXEC fallback guard used in mount.c and
    fuse_loop_mt.c for portability to older headers.
    
    Signed-off-by: Abhinav Agarwal <abhinavagarwal1996@gmail.com>

--- lib/fuse_lowlevel.c
+++ lib/fuse_lowlevel.c
@@ -51,6 +51,10 @@
 #define F_SETPIPE_SZ	(F_LINUX_SPECIFIC_BASE + 7)
 #endif
 
+#ifndef O_CLOEXEC
+#define O_CLOEXEC 0
+#endif
+
 #define PARAM(inarg) (((char *)(inarg)) + sizeof(*(inarg)))
 #define OFFSET_MAX 0x7fffffffffffffffLL
 
@@ -796,7 +800,7 @@ static int grow_pipe_to_max(int pipefd)
 	long maxfd;
 	char buf[32];
 
-	maxfd = open("/proc/sys/fs/pipe-max-size", O_RDONLY);
+	maxfd = open("/proc/sys/fs/pipe-max-size", O_RDONLY | O_CLOEXEC);
 	if (maxfd < 0)
 		return -errno;
 
