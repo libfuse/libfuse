29872737535a6f2cb39902fb112c3665f75e15f0
Darrick J. Wong <djwong@kernel.org>
2026-05-05 16:10:59 -0700

    libfuse: don't use SYNC_INIT unless asked for
    fuse2fs calls fuse_main, then starts threads from ->init.  It doesn't
    call fuse_daemonize_early_start because I haven't ported it to use any
    of the new APIs.  I don't have io_uring enabled for fuse on my dev box.
    
    fuse_main calls fuse_session_mount calls fuse_session_mount_new_api
    calls session_start_sync_init.  At the start of the function,
    se->want_sync_init, se->uring.enable, and daemonize.active are all
    false.  The first branch is not taken, so we call FUSE_DEV_IOC_SYNC_INIT
    and enable sync_init even though the user didn't ask for that and didn't
    prepare for it either.
    
    FUSE_DEV_IOC_SYNC_INIT succeeds, so we send the synchronous FUSE_INIT
    from mount, which calls fuse2fs' init() method.  That starts the
    background threads and returns.  Upon return to the kernel, the mount()
    now succeeds, and the next thing that fuse_main does is call
    fuse_daemonize().  Since we didn't call fuse_daemonize_early_start, the
    daemonize forks the process and the threads die with the parent.
    
    If we didn't ask for SYNC_INIT, don't enable it.  This is needed to
    maintain compatibility with older fuse servers that only support
    asynchronous FUSE_INIT.
    
    Fixes: 3e1101057aea ("fuse mount: Support synchronous FUSE_INIT (privileged daemon)")
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Reviewed-by: Bernd Schubert <bernd@bsbernd.com>
    
    --
    
    v1.2: simplify the logic even more
    v1.1: improve commit message, refine logging logic

--- lib/fuse_lowlevel.c
+++ lib/fuse_lowlevel.c
@@ -4475,8 +4475,11 @@ static int session_start_sync_init(struct fuse_session *se, int fd)
 {
 	int err, res;
 
-	if (!se->want_sync_init &&
-		(se->uring.enable && !fuse_daemonize_is_used())) {
+	/*
+	 * Older fuse servers do not set want_sync_init or start the new
+	 * daemonize code, so they get async init.
+	 */
+	if (!fuse_daemonize_is_used() || !se->want_sync_init) {
 		if (se->debug)
 			fuse_log(FUSE_LOG_DEBUG,
 					"fuse: sync init not enabled\n");
