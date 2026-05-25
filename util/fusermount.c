libfuse/libfuse

commit 50fa7f0bc2df71b5d79a3501a95855d2d7da2338

Move fuse_mnt_build_{source,type} to mount_util.c
Also make it more generic and avoid taking struct mount_opts
as parameter.

mount_fsmount.c and fusermount.c use these functions now.

Also use asprintf as suggested by Darrick.
For safety also free source and type in fuse_kern_do_mount()
when subtype is not supported - right now it would be safe
because without subtype the allocated memory is just a bit too
large, but would easy to break in future commits.

Signed-off-by: Bernd Schubert <bernd@bsbernd.com>

util/fusermount.c
@@ -1045,24 +1045,12 @@ static int prepare_mount(const char *opts, struct mount_params *mp)
 	sprintf(d, "fd=%i,rootmode=%o,user_id=%u,group_id=%u",
 		mp->fd, mp->rootmode, getuid(), getgid());
 
-	mp->source = malloc((mp->fsname ? strlen(mp->fsname) : 0) +
-			(mp->subtype ? strlen(mp->subtype) : 0) + strlen(mp->dev) + 32);
-
-	mp->type = malloc((mp->subtype ? strlen(mp->subtype) : 0) + 32);
+	mp->source = fuse_mnt_build_source(mp->fsname, mp->subtype, mp->dev);
+	mp->type = fuse_mnt_build_type(mp->blkdev, mp->subtype);
 	if (!mp->type || !mp->source) {
 		fprintf(stderr, "%s: failed to allocate memory\n", progname);
 		goto err;
 	}
-
-	if (mp->subtype)
-		sprintf(mp->type, "%s.%s", mp->blkdev ? "fuseblk" : "fuse", mp->subtype);
-	else
-		strcpy(mp->type, mp->blkdev ? "fuseblk" : "fuse");
-
-	if (mp->fsname)
-		strcpy(mp->source, mp->fsname);
-	else
-		strcpy(mp->source, mp->subtype ? mp->subtype : mp->dev);
 
 	return 0;
 

lib/mount.c
@@ -651,26 +651,38 @@ int fuse_kern_do_mount(const char *mnt, struct mount_opts *mo,
 	char *source = NULL;
 	char *type = NULL;
 	int res;
+	const char *devname = fuse_mnt_get_devname();
 
 	res = -ENOMEM;
-	source = fuse_mnt_build_source(mo);
-	type = fuse_mnt_build_type(mo);
+	source = fuse_mnt_build_source(mo->fsname, mo->subtype, devname);
+	type = fuse_mnt_build_type(mo->blkdev, mo->subtype);
 	if (!type || !source) {
-		fuse_log(FUSE_LOG_ERR, "fuse: failed to allocate memory\n");
+		fuse_log(FUSE_LOG_ERR, "%s: failed to allocate memory\n",
+			 __func__);
 		goto out_close;
 	}
 
 	res = mount(source, mnt, type, mo->flags, mo->kernel_opts);
 	if (res == -1 && errno == ENODEV && mo->subtype) {
 		/* Probably missing subtype support */
-		strcpy(type, mo->blkdev ? "fuseblk" : "fuse");
-		if (mo->fsname) {
-			if (!mo->blkdev)
-				sprintf(source, "%s#%s", mo->subtype,
-					mo->fsname);
-		} else {
-			strcpy(source, type);
+
+		/*
+		 * The allocated space by fuse_mnt_build_{source,type}
+		 * might be too small.
+		 */
+		free(source);
+		free(type);
+
+		type = fuse_mnt_build_type(mo->blkdev, NULL);
+		source = fuse_mnt_build_source(mo->fsname, NULL, devname);
+
+		if (!type || !source) {
+			fuse_log(FUSE_LOG_ERR,
+				 "%s: failed to allocate memory\n",
+				 __func__);
+			goto out_close;
 		}
+
 		res = mount(source, mnt, type, mo->flags, mo->kernel_opts);
 	}
 	if (res == -1) {
@@ -833,38 +845,4 @@ int fuse_kern_mount(const char *mountpoint, struct mount_opts *mo)
 out:
 	free(mnt_opts);
 	return res;
-}
-
-char *fuse_mnt_build_source(const struct mount_opts *mo)
-{
-	const char *devname = fuse_mnt_get_devname();
-	char *source;
-
-	source = malloc((mo->fsname ? strlen(mo->fsname) : 0) +
-			(mo->subtype ? strlen(mo->subtype) : 0) +
-			strlen(devname) + 32);
-	if (!source)
-		return NULL;
-
-	strcpy(source,
-	       mo->fsname ? mo->fsname : (mo->subtype ? mo->subtype : devname));
-
-	return source;
-}
-
-char *fuse_mnt_build_type(const struct mount_opts *mo)
-{
-	char *type;
-
-	type = malloc((mo->subtype ? strlen(mo->subtype) : 0) + 32);
-	if (!type)
-		return NULL;
-
-	strcpy(type, mo->blkdev ? "fuseblk" : "fuse");
-	if (mo->subtype) {
-		strcat(type, ".");
-		strcat(type, mo->subtype);
-	}
-
-	return type;
 }

lib/mount_fsmount.c
@@ -355,15 +355,21 @@ int fuse_kern_fsmount(const char *mnt, unsigned long flags, int blkdev,
 		      const char *source_dev, const char *kernel_opts,
 		      const char *mnt_opts)
 {
-	const char *type;
+	char *type = NULL;
 	char *source = NULL;
 	int fsfd = -1;
 	int mntfd = -1;
 	int err, res;
 	unsigned long mount_attrs;
 
-	/* Determine filesystem type */
-	type = blkdev ? "fuseblk" : "fuse";
+	/* Build type and source strings */
+	type = fuse_mnt_build_type(blkdev, subtype);
+	source = fuse_mnt_build_source(fsname, subtype, source_dev);
+	err = -ENOMEM;
+	if (!type || !source) {
+		fprintf(stderr, "fuse: failed to allocate memory\n");
+		goto out_free;
+	}
 
 	/* Try to open filesystem context */
 	fsfd = fsopen(type, FSOPEN_CLOEXEC);
@@ -371,20 +377,8 @@ int fuse_kern_fsmount(const char *mnt, unsigned long flags, int blkdev,
 		if (errno != EPERM)
 			fprintf(stderr, "fuse: fsopen(%s) failed: %s\n", type,
 				strerror(errno));
-		return -1;
+		goto out_free;
 	}
-
-	/* Build source string */
-	source = malloc((fsname ? strlen(fsname) : 0) +
-			(subtype ? strlen(subtype) : 0) +
-			strlen(source_dev) + 32);
-	err = -ENOMEM;
-	if (!source) {
-		fprintf(stderr, "fuse: failed to allocate memory\n");
-		goto out_close_fsfd;
-	}
-
-	strcpy(source, fsname ? fsname : (subtype ? subtype : source_dev));
 
 	/* Configure source */
 	res = fsconfig(fsfd, FSCONFIG_SET_STRING, "source", source, 0);
@@ -468,6 +462,7 @@ int fuse_kern_fsmount(const char *mnt, unsigned long flags, int blkdev,
 
 	close(mntfd);
 	free(source);
+	free(type);
 	return 0;
 
 out_umount:
@@ -487,7 +482,7 @@ out_close_mntfd:
 		close(mntfd);
 out_free:
 	free(source);
-out_close_fsfd:
+	free(type);
 	if (fsfd != -1)
 		close(fsfd);
 	errno = -err;

lib/mount_i_linux.h
@@ -53,8 +53,6 @@ int fuse_kern_fsmount(const char *mnt, unsigned long flags, int blkdev,
 
 int fuse_kern_fsmount_mo(const char *mnt, const struct mount_opts *mo,
 			 const char *mnt_opts);
-char *fuse_mnt_build_source(const struct mount_opts *mo);
-char *fuse_mnt_build_type(const struct mount_opts *mo);
 int mount_fusermount_obtain_fd(const char *mountpoint,
 			       struct mount_opts *mo,
 			       const char *opts, int *sock_fd_out,

lib/mount_util.c
@@ -463,3 +463,34 @@ const char *fuse_mnt_get_devname(void)
 
 	return devname ? devname : "/dev/fuse";
 }
+
+char *fuse_mnt_build_source(const char *fsname, const char *subtype,
+			     const char *devname)
+{
+	char *source;
+	int ret;
+
+	ret = asprintf(&source, "%s",
+		       fsname ? fsname : (subtype ? subtype : devname));
+	if (ret == -1)
+		return NULL;
+
+	return source;
+}
+
+char *fuse_mnt_build_type(int blkdev, const char *subtype)
+{
+	char *type;
+	int ret;
+
+	if (subtype)
+		ret = asprintf(&type, "%s.%s", blkdev ? "fuseblk" : "fuse",
+			       subtype);
+	else
+		ret = asprintf(&type, "%s", blkdev ? "fuseblk" : "fuse");
+
+	if (ret == -1)
+		return NULL;
+
+	return type;
+}

lib/mount_util.h
@@ -36,4 +36,9 @@ const char *fuse_mnt_get_devname(void);
 int fuse_mnt_add_mount_helper(const char *mnt, const char *source,
 			       const char *type, const char *mnt_opts);
 
+/* Build source and type strings for mounting */
+char *fuse_mnt_build_source(const char *fsname, const char *subtype,
+			     const char *devname);
+char *fuse_mnt_build_type(int blkdev, const char *subtype);
+
 #endif /* FUSE_MOUNT_UTIL_H_ */

commit 631cbc93a75cb58d390a58436a22d974c08f93a6

fusermount: Fix heap buffer overflow in perform_mount()
Commit 50fa7f0bc2df ("Move fuse_mnt_build_{source,type} to mount_util.c"
introduced a heap buffer overflow in the ENODEV fallback
path in fusermount.c. The prepare_mount() function now allocates source
with only strlen(fsname) + 1 bytes via fuse_mnt_build_source(), but
perform_mount() writes strlen(subtype) + 1 + strlen(fsname) + 1 bytes
when building the legacy "subtype#fsname" format.

Fix by removing source/type from struct mount_params and building them
as local variables in perform_mount() with correct allocation using the
new use_subtype_prefix parameter added in the previous commit.

The source and type strings are now:
- Built in perform_mount() with proper size allocation
- Returned via output parameters to the caller
- No longer stored in the mount_params structure

Fixes: 50fa7f0bc2df ("Move fuse_mnt_build_{source,type} to mount_util.c")
Signed-off-by: Bernd Schubert <bernd@bsbernd.com>

util/fusermount.c
@@ -913,10 +913,6 @@ struct mount_params {
 	char *subtype;          /* Subtype from options */
 	int blkdev;             /* Block device flag */
 
-	/* Generated mount parameters */
-	char *source;           /* Mount source string */
-	char *type;             /* Filesystem type string */
-
 	/* mtab/utab record content: MS_* flag mirrors + kernel-bound -o opts
 	 * + user=<n> (non-root)
 	 */
@@ -931,8 +927,6 @@ static void free_mount_params(struct mount_params *mp)
 	free(mp->optbuf);
 	free(mp->fsname);
 	free(mp->subtype);
-	free(mp->source);
-	free(mp->type);
 	free(mp->mtab_opts);
 	memset(mp, 0, sizeof(*mp));
 }
@@ -1056,13 +1050,6 @@ static int prepare_mount(const char *opts, struct mount_params *mp)
 	sprintf(d, "fd=%i,rootmode=%o,user_id=%u,group_id=%u",
 		mp->fd, mp->rootmode, getuid(), getgid());
 
-	mp->source = fuse_mnt_build_source(mp->fsname, mp->subtype, mp->dev);
-	mp->type = fuse_mnt_build_type(mp->blkdev, mp->subtype);
-	if (!mp->type || !mp->source) {
-		fprintf(stderr, "%s: failed to allocate memory\n", progname);
-		goto err;
-	}
-
 	return 0;
 
 err:
@@ -1072,31 +1059,54 @@ err:
 
 /*
  * Perform the actual mount operation using prepared parameters.
+ * Builds source and type strings and returns them via output parameters.
  *
  * Returns 0 on success, -1 on failure.
  */
-static int perform_mount(const char *mnt, struct mount_params *mp)
+static int perform_mount(const char *mnt, struct mount_params *mp,
+			 char **source_out, char **type_out)
 {
 	int res;
+	char *source = NULL;
+	char *type = NULL;
 
-	res = mount_notrunc(mp->source, mnt, mp->type, mp->flags, mp->optbuf);
+	source = fuse_mnt_build_source(mp->fsname, mp->subtype, mp->dev, 0);
+	type = fuse_mnt_build_type(mp->blkdev, mp->subtype);
+	if (!type || !source) {
+		fprintf(stderr, "%s: failed to allocate memory\n", progname);
+		free(source);
+		free(type);
+		return -1;
+	}
+
+	res = mount_notrunc(source, mnt, type, mp->flags, mp->optbuf);
 	if (res == -1 && errno == ENODEV && mp->subtype) {
 		/* Probably missing subtype support */
-		strcpy(mp->type, mp->blkdev ? "fuseblk" : "fuse");
-		if (mp->fsname) {
-			if (!mp->blkdev)
-				sprintf(mp->source, "%s#%s", mp->subtype, mp->fsname);
+		free(source);
+		free(type);
+
+		type = fuse_mnt_build_type(mp->blkdev, NULL);
+		if (mp->fsname && !mp->blkdev) {
+			source = fuse_mnt_build_source(mp->fsname, mp->subtype,
+						       mp->dev, 1);
 		} else {
-			strcpy(mp->source, mp->type);
+			source = strdup(type);
 		}
 
-		res = mount_notrunc(mp->source, mnt, mp->type, mp->flags, mp->optbuf);
+		if (!type || !source) {
+			fprintf(stderr, "%s: failed to allocate memory\n", progname);
+			free(source);
+			free(type);
+			return -1;
+		}
+
+		res = mount_notrunc(source, mnt, type, mp->flags, mp->optbuf);
 	}
 	if (res == -1 && errno == EINVAL) {
 		/* It could be an old version not supporting group_id */
 		sprintf(mp->optbuf_end, "fd=%i,rootmode=%o,user_id=%u",
 			mp->fd, mp->rootmode, getuid());
-		res = mount_notrunc(mp->source, mnt, mp->type, mp->flags, mp->optbuf);
+		res = mount_notrunc(source, mnt, type, mp->flags, mp->optbuf);
 	}
 	if (res == -1) {
 		int errno_save = errno;
@@ -1106,9 +1116,13 @@ static int perform_mount(const char *mnt, struct mount_params *mp)
 		else
 			fprintf(stderr, "%s: mount failed: %s\n", progname,
 				strerror(errno_save));
+		free(source);
+		free(type);
 		return -1;
 	}
 
+	*source_out = source;
+	*type_out = type;
 	return 0;
 }
 
@@ -1118,6 +1132,8 @@ static int do_mount(const char *mnt, const char **typep, mode_t rootmode,
 {
 	struct mount_params mp = { .fd = fd }; /* implicit zero of other params */
 	int res;
+	char *source = NULL;
+	char *type = NULL;
 
 	mp.rootmode = rootmode;
 	mp.dev = dev;
@@ -1126,14 +1142,14 @@ static int do_mount(const char *mnt, const char **typep, mode_t rootmode,
 	if (res == -1)
 		return -1;
 
-	res = perform_mount(mnt, &mp);
+	res = perform_mount(mnt, &mp, &source, &type);
 	if (res == -1) {
 		free_mount_params(&mp);
 		return -1;
 	}
 
-	*sourcep = mp.source;
-	*typep = mp.type;
+	*sourcep = source;
+	*typep = type;
 	*mtab_optsp = mp.mtab_opts;
 
 	/* Free only the intermediate allocations, not the returned ones */
@@ -1436,10 +1452,16 @@ static int mount_fuse_finish_fsmount(const char *mnt,
 		goto fail_free_merged;
 	}
 
-	/* Store results in context */
-	ctx->source = mp.source;
+	/* Build source and type for mtab after successful mount */
+	ctx->source = fuse_mnt_build_source(mp.fsname, mp.subtype, ctx->dev, 0);
+	*type = fuse_mnt_build_type(mp.blkdev, mp.subtype);
+	if (!*type || !ctx->source) {
+		fprintf(stderr, "%s: failed to allocate memory\n", progname);
+		goto fail_free_merged;
+	}
+
+	/* Store mtab_opts in context */
 	ctx->mtab_opts = final_mtab_opts;
-	*type = mp.type;
 
 	res = 0;
 